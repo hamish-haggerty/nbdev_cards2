c461585d88c20f12bf1055c5a559d5c3fe12e449
Hamish Haggerty <hamishhaggerty@Hamishs-MacBook-Air.local>
2022-10-17 13:32:22 +1100

    ok hoefully now

--- nbdev_cards2/core.py
+++ nbdev_cards2/core.py
@@ -8,9 +8,9 @@ import torch
 if torch.cuda.is_available():device='cuda'
 else:device='cpu'
 import random
-import os
-import numpy as np
-import math
+# import os
+# import numpy as np
+# import math
 
 # %% ../nbs/00_core.ipynb 4
 def foo(): pass
